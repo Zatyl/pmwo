d35052f5181c2c3b73bd39be1a1b2fade37af1e6
IceVan <bozga@loken.pl>
2017-11-10 12:10:50 +0100

    aplikacja +schematy
    Baza aplikacji + schematy

---
+++ src/main/java/com/pwr/library/LibraryOnnline/LibraryOnnlineApplication.java
@@ -0,0 +1,12 @@
+package com.pwr.library.LibraryOnnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryOnnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryOnnlineApplication.class, args);
+	}
+}
